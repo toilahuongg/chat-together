a503e07d94b38347d548c8fbb1004d6d69315a6f
duonggiang152 <62105435+duonggiang152@users.noreply.github.com>
2022-03-09 09:51:23 +0700

    feat: add route get room

--- server/routes/room.route.ts
+++ server/routes/room.route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import UserModel from '../models/user.model';
 import RoomModel from '../models/room.model';
 
 const Router = express.Router();
@@ -21,6 +22,33 @@ Router.post('/api/room/:id', passport.authenticate('jwt', { session: false  }), 
     }
     return res.json();
 });
-
+/**
+ * Lấy về toàn bộ room mà user tham gia
+ */
+Router.get('/api/getroompaticipate',passport.authenticate('jwt', { session: false  }), async (req, res) => {
+    // kiểm tra user
+    try{
+        if(!req.auth) {
+            res.status(401)
+            return res.send("unauthentication")
+        }
+        const userID:string = req.auth._id.toString()
+        // kiểm tra user có tồn tại hay không
+        const user = UserModel.findOne({_id: userID})
+        if(!user) {
+            res.status(404)
+            return res.send({nessage: "Lỗi"})
+        }
+        // lấy phòng
+        const rooms =await RoomModel.find({userIDs: {"$in" : `${userID}`}}).sort({ createdAt: -1})
+        console.log(rooms)
+        res.status(200)
+        return res.send(rooms)
+    } catch(err) {
+        console.log(err)
+        res.status(404)
+        return res.send({nessage: "Lỗi"})
+    }
+})
 
 export default Router;
