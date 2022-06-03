6bec3cb93ebe35f5e409ffbe42be864efbbd26b5
toilahuong <vbh30062001@gmail.com>
2022-06-03 06:26:50 +0000

    fix: ui

--- src/Components/Guest/GuestLayout.tsx
+++ src/Components/Guest/GuestLayout.tsx
@@ -4,6 +4,7 @@ import Slider from './Slider';
 
 import Or from '@src/styles/svg/or.svg';
 import styles from './client.module.scss';
+import useWindowSize from '@src/hooks/useWindowSize';
 
 type TProps = {
 	children: ReactNode,
@@ -11,27 +12,33 @@ type TProps = {
 	errorMessage?: string
 }
 const GuestLayout: React.FC<TProps> = ({ children, isRegister, errorMessage }) => {
-	
+	const size = useWindowSize();
 	return (
-		<div className={styles.wrapper}>
-			<div>
-				<div className={styles.container}>
-					{ children}
-					{errorMessage && <p className={styles.errorMessage}> {errorMessage} </p> }
-					<div className={styles.or}>
-						<div><Or /></div>
-						{ !isRegister ? (<p> Bạn chưa có tài khoản? <Link href="/register"> Đăng ký</Link></p>):
-							(<p> Bạn đã có tài khoản? <Link href="/login"> Đăng nhập</Link></p>)
-						}
+		<>
+			<h1 className={styles.title}> CHAT TOGETHER</h1>
+			<div className={styles.wrapper}>
+				<div>
+					<div className={styles.container}>
+						{children}
+						{errorMessage && <p className={styles.errorMessage}> {errorMessage} </p>}
+						<div className={styles.or}>
+							<div><Or /></div>
+							{!isRegister ? (<p> Bạn chưa có tài khoản? <Link href="/register"> Đăng ký</Link></p>) :
+								(<p> Bạn đã có tài khoản? <Link href="/login"> Đăng nhập</Link></p>)
+							}
+						</div>
 					</div>
 				</div>
+				{size.width > 768 && (
+					<div>
+						<div className="slider">
+							<Slider />
+						</div>
+					</div>
+				)}
 			</div>
-			<div>
-				<div className="slider">
-					<Slider />
-				</div>
-			</div>
-		</div>
+		</>
+
 	)
 }
 
